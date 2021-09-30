ce19d14ecec45c602045a178c048cc2ca666387f
kiennguyenGH <ktnguyen4647@gmail.com>
2021-09-30 03:17:25 -0700

    Add number_game.c and restaurant_bill.c; Add details to outline

---
+++ outline.c
@@ -0,0 +1,12 @@
+/* Restauraunt Bill
+- Declare 4 floats and assign prices for salad, soup, sandwich, and pizza in an array
+- Randomly generate a number between 0-3, which will be the index of the array
+- The user will be asked to enter the to input two integers as percentages, one for tax, and the other for tip
+- Calculate function will calculate the cost of the meal, returning the total bill as a float.
+*/
+
+/* Number Guessing Game
+- Declare an int x and assign it to a number that != 3
+- Create a while loop that will break if x == 3
+- In the while loop, use int pointer to 
+*/
